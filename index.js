0151210defc4972019ffd272c00e26b5ab5abaf3
follyb2810 <babatundeyusuffolorunsho@gmail.com>
2023-11-25 01:00:16 -0800

    adding changes

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const env = require('dotenv');
-const cors = require('cors')
+const cors = require('cors');
 const emailModel = require('./model/EmailModel');
 const connectDB = require('./Config/db');
-
 
 env.config();
 const app = express();
@@ -11,31 +10,55 @@ app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 
 app.get('/', (req, res) => {
-  res.send('hello');
+  res.send('Hello');
 });
 
 app.post('/email', async (req, res) => {
-  const { email,name,message } = req.body;
+  const { email, name, message } = req.body;
+
   if (!isValidEmail(email)) {
-    return res.status(400).json({ message: 'Invalid email format' });
+    return res.status(400).json({ error: 'Invalid email format' });
   }
+
   try {
-    if(!email || !name || !message) return res.status(401).json({message:'Please, all input are require'})
-    const newEmail = new emailModel({ email,name,message });
+    if (!email || !name || !message) {
+      return res.status(400).json({ error: 'All input fields are required' });
+    }
+
+    const newEmail = new emailModel({ email, name, message });
 
     await newEmail.save();
 
-    res.status(200).json({ message:"Thank you for contacting us!." });
+    res.status(200).json({ message: 'Thank you for contacting us!' });
   } catch (error) {
-    console.error('Please Provide a valid Email:', error);
-    res.status(500).json({ message: 'Error saving email' });
+    console.error('Error saving email:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.post('/newsletter', async (req, res) => {
+  const { email } = req.body;
+
+  if (!isValidEmail(email)) {
+    return res.status(400).json({ error: 'Invalid email format' });
+  }
+
+  try {
+    const newEmail = new emailModel({ email });
+
+    await newEmail.save();
+
+    res.status(200).json({ message: 'Thank you for subscribing to our Newsletter!' });
+  } catch (error) {
+    console.error('Error saving email:', error);
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
